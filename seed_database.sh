eee41e3f508e2466995bea24b4c4c497da484aba
Dakota Seagraves <dakotaseagraves31@gmail.com>
2026-05-06 15:15:02 -0500

    Initial commit

---
+++ seed_database.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm db.sqlite3
+rm -rf ./adventurerscodexapi/migrations
+python3 manage.py migrate
+python3 manage.py makemigrations adventurerscodexapi
+python3 manage.py migrate adventurerscodexapi
+python3 manage.py loaddata users
+python3 manage.py loaddata tokens
+
