f600dd172f9f54c19501c96ce8744f42bde25ef8
Dakota Seagraves <dakotaseagraves31@gmail.com>
2026-05-13 15:52:03 -0500

    Re-export get_current_user from views package

--- adventurerscodexapi/views/__init__.py
+++ adventurerscodexapi/views/__init__.py
@@ -1,4 +1,4 @@
-from .auth import login_user, register_user
+from .auth import get_current_user, login_user, register_user
 from .character import CharacterSerializer, CharacterViewSet
 from .dnd_class import DndClassSerializer, DndClassViewSet
 from .race import RaceSerializer, RaceViewSet
