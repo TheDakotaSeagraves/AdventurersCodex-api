a4538f688df96fdb5c3fce66ac09a1d1ef11865b
Dakota Seagraves <dakotaseagraves31@gmail.com>
2026-05-15 13:43:45 -0500

    Add CharacterClassLevel model

--- adventurerscodexapi/admin.py
+++ adventurerscodexapi/admin.py
@@ -2,8 +2,9 @@
 
 from django.contrib import admin
 
-from .models import Character, DndClass, Race
+from .models import Character, CharacterClassLevel, DndClass, Race
 
 admin.site.register(Character)
+admin.site.register(CharacterClassLevel)
 admin.site.register(DndClass)
 admin.site.register(Race)
--- adventurerscodexapi/models/__init__.py
+++ adventurerscodexapi/models/__init__.py
@@ -1,3 +1,4 @@
 from .character import Character
+from .character_class_level import CharacterClassLevel
 from .dnd_class import DndClass
 from .race import Race
